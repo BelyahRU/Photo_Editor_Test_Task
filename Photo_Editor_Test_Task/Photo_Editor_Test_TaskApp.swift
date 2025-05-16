e07335abab80af34bcb25a8540cdb61da474f028
Alexander <oBelyash@yandex.ru>
2025-05-16 18:42:01 +0300

    Initial Commit

---
+++ Photo_Editor_Test_Task/Photo_Editor_Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Editor_Test_TaskApp.swift
+//  Photo_Editor_Test_Task
+//
+//  Created by Александр Андреев on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Editor_Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
